40cc8fa11ebd213c3d11368d5e255d46f091d0eb
cybercy1234-cyber <cybercy1234@gmail.com>
2026-05-12 12:23:59 +0700

    Time: 0 ms (100%), Space: 12.5 MB (0%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def toGoatLatin(self, sentence):
+        words = sentence.split()
+        vowels = set("aeiouAEIOU")
+        result = []
+
+        for i, word in enumerate(words):
+            if word[0] in vowels:
+                new_word = word + "ma"
+            else:
+                new_word = word[1:] + word[0] + "ma"
+            
+            # thêm 'a' theo vị trí (i bắt đầu từ 0 nên +1)
+            new_word += "a" * (i + 1)
+            
+            result.append(new_word)
+
+        return " ".join(result)
