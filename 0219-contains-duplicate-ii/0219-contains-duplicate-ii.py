3ffe8962627311a46c5cfe3c4194cd9999325984
cybercy1234-cyber <cybercy1234@gmail.com>
2026-05-12 12:40:41 +0700

    Time: 23 ms (98.35%), Space: 23.9 MB (84.1%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        seen = {}
+
+        for i, x in enumerate(nums):
+
+            if x in seen and i - seen[x] <= k:
+                return True
+
+            seen[x] = i
+
+        return False
